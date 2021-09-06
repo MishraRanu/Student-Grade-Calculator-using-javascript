cf971082feca7e89ca8b7e4db554a1833f435c3c
Ranu311 <71586938+Ranu311@users.noreply.github.com>
2021-09-06 11:41:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+const btn=document.getElementById('btn')
 
+const calculate=0=>{
+  let web=document.getElementById('web').value
+  let phy=document.getElementById('phy').value
+  let chem=document.getElementById('chem').value
+  let maths=document.getElementById('maths').value
+  let obtainedMarks=parseFloat(web)+parseFloat(phy)+parseFloat(chem)+parseFloat(maths)
+  
+  let percentage=(obtainedMarks/400)*100
+  if(percentage<=100 && percentage>=80)
+  {
+    grade='A'
+  }
+  else if(percentage<=79 && percentage>=60)
+  {
+    grade='B'
+  }
+  else if(percentage<=59 && percentage>=40)
+  {
+    grade='C'
+  }
+  else if(percentage<=39 && percentage>=33)
+  {
+    grade='D'
+  }
+  else{
+grade='F'
+  }
+  if(percentage>=33)
+  {
+let showResult=document.getElementById('showResult')
+showResult.innerHTML="You have obtained $(obtainedMarks)out of 400 $(percentage)% and $(grade). You are pass."
+}
+  else{
+    let showResult=document.getElementById('showResult')
+    showResult.innerHTML="You have obtained $(obtainedMarks) out of 400 with $(percentage)% and $(grade). You are Fail"
+  }
+}
+
+
+
+
+
+
+btn.addEventListener('click',calculate)
