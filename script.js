a4b1a6be5b97a0c4ab580cc2c2e77938457b9950
Ranu311 <71586938+Ranu311@users.noreply.github.com>
2021-09-06 11:51:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-const btn=document.getElementById('btn')
+const btn = document.getElementById('btn')
 
-const calculate=0=>{
-  let web=document.getElementById('web').value
-  let phy=document.getElementById('phy').value
-  let chem=document.getElementById('chem').value
-  let maths=document.getElementById('maths').value
-  let obtainedMarks=parseFloat(web)+parseFloat(phy)+parseFloat(chem)+parseFloat(maths)
-  
-  let percentage=(obtainedMarks/400)*100
-  let grade=' '
-  if(percentage<=100 && percentage>=80)
-  {
-    grade='A'
+// Function
+const calculate = () => {
+  let web = document.getElementById('web').value
+  let phy = document.getElementById('phy').value
+  let chem = document.getElementById('chem').value
+  let maths = document.getElementById('maths').value
+
+  let obtainedMarks =
+    parseFloat(web) + parseFloat(phy) + parseFloat(chem) + parseFloat(maths)
+
+  let percentage = (obtainedMarks / 400) * 100
+
+  let grade = ''
+
+  if (percentage <= 100 && percentage >= 80) {
+    grade = 'A'
+  } else if (percentage <= 79 && percentage >= 60) {
+    grade = 'B'
+  } else if (percentage <= 59 && percentage >= 40) {
+    grade = 'C'
+  } else if (percentage <= 39 && percentage >= 33) {
+    grade = 'D'
+  } else {
+    grade = 'F'
   }
-  else if(percentage<=79 && percentage>=60)
-  {
-    grade='B'
-  }
-  else if(percentage<=59 && percentage>=40)
-  {
-    grade='C'
-  }
-  else if(percentage<=39 && percentage>=33)
-  {
-    grade='D'
-  }
-  else{
-grade='F'
-  }
-  if(percentage>=33)
-  {
-let showResult=document.getElementById('showResult')
-showResult.innerHTML="You have obtained $(obtainedMarks)out of 400 $(percentage)% and $(grade). You are pass."
-}
-  else{
-    let showResult=document.getElementById('showResult')
-    showResult.innerHTML="You have obtained $(obtainedMarks) out of 400 with $(percentage)% and $(grade). You are Fail"
+
+  if (percentage >= 33) {
+    let showResult = document.getElementById('showResult')
+    showResult.innerHTML = `You have obtained ${obtainedMarks} Marks out of 400 Marks with ${percentage}% <br>and ${grade} Grade.You Are Pass.`
+  } else {
+    let showResult = document.getElementById('showResult')
+    showResult.innerHTML = `You have obtained ${obtainedMarks} Marks out of 400 Marks with ${percentage}% <br>and ${grade} Grade.You Are Fail.`
   }
 }
 
-
-
-
-
-
-btn.addEventListener('click',calculate)
+// Event Listener
+btn.addEventListener('click', calculate)
